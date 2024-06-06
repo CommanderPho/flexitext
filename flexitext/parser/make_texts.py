cf910465b7b28083becc4a0dbc97c4c7ee3a9068
Pho Hale <PhoHale@gmail.com>
2024-06-05 20:25:09 -0400

    make_plaintext_string

--- flexitext/parser/make_texts.py
+++ flexitext/parser/make_texts.py
@@ -1,10 +1,29 @@
+from typing import List
 from .blocks import BlockParser, BlockScanner
 
 
-def make_texts(string):
+def make_texts(string) -> List:
     scanner = BlockScanner(string)
     scanner.scan()
     parser = BlockParser(scanner.tokens)
     parser.parse()
     texts = parser.texts
     return texts
+
+
+
+def make_plaintext_string(formatted_string: str) -> str:
+    """ Strips the formatting from a provided formatted_string and returns the unformatted plaintext content.
+
+    Usage:
+
+    from flexitext.parser.make_texts import make_texts, make_plaintext_string
+    
+    plaintext_title: str = make_plaintext_string(formatted_string=title)
+    plaintext_title
+    """
+    if formatted_string is None:
+        return None
+    ## strip formatting to convert back to plaintext string:
+    title_texts_obj: List = make_texts(formatted_string) # Text
+    return ''.join([v.string for v in title_texts_obj])
